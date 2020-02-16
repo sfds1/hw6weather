9f78170f794094fa64036d566581ab80899eef98
SK <stephanie@sfdigitalstudio.com>
2020-02-15 21:41:19 -0800

    added UV Index

--- assets/script.js
+++ assets/script.js
@@ -2,16 +2,17 @@ $("#submit").on("click", function(event){
     event.preventDefault();
     let inputCity = $("#city").val();
     let inputState = $("#state").val(); 
-    let today = new Date();
-    // let currentDay = Date.prototype.toDateString();
-    console.log(today);
+    let today = new Date().toDateString();
+
  
- 
+    if (inputCity === '' || inputState === ''){
+        alert("Please enter a city and a state.");
+    } else {
  // Class API key for openweathermap 
  var APIKey = "166a433c57516f51dfab1f7edaed8413";
 
 
- // The URL we need to query the database
+ // The URL to get the current weather
  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + inputCity + "," + inputState + ",US&appid=" + APIKey;
 
  // We then created an AJAX call
@@ -26,13 +27,63 @@ $("#submit").on("click", function(event){
    // Create CODE HERE to transfer content to HTML
    $('.date').text(today);
    $('.city').text(`${response.name} Weather details`);
-   $('.humidity').text(`Humidity: ${response.main.humidity}`);
-   $('.wind').text(`Wind speed: ${response.wind.speed}`);
+   $('.humidity').text(`Humidity: ${response.main.humidity}%`);
+   $('.wind').text(`Wind speed: ${response.wind.speed} MPH`);
    // Create CODE HERE to calculate the temperature (converted from Kelvin)
    const f = (response.main.temp - 273.15) * 1.8 + 32;
    // Hint: To convert from Kelvin to Fahrenheit: F = (K - 273.15) * 1.80 + 32
  //   SK - use toFixed to make it go to 2 decimal places
-   $('.temp').text(`Temp in Fahrenheit: ${f.toFixed(2)}`);
+   $('.temp').text(`Temperature: ${f.toFixed(2)} F`);
    // Create CODE HERE to dump the temperature content into HTML
+
+
+// nested AJAX call to get the UV Index using long and lat from first API call
+        var lat = response.coord.lat;
+        var long = response.coord.lon;
+        console.log(lat);
+        console.log(long);
+
+        var queryURLUV =  "http://api.openweathermap.org/data/2.5/uvi?appid=" + APIKey + "&lat=" + lat + "&lon=" + long;
+
+        console.log(queryURLUV);
+        $.ajax({
+            url: queryURLUV,
+            method: "GET"
+          }).then(function(responseUV) {
+              console.log (responseUV);
+            $('.uvIndex').text(`UV Index: ${responseUV.value}`);
+
+          });
+
+
  });
+
+
+
+// // The URL to get the 5 day forecast weather
+// var queryURLForecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + inputCity + "," + inputState + ",US&appid=" + APIKey;
+
+// // We then created an AJAX call
+// $.ajax({
+//   url: queryURLForecast,
+//   method: "GET"
+// }).then(function(responseForecast) {
+//   // Create CODE HERE to Log the queryURL
+//   console.log(queryURLForecast);
+//   console.log(responseForecast);      
+//   // Create CODE HERE to log the resulting object
+//   // Create CODE HERE to transfer content to HTML
+//   $('.date').text(today);
+//   $('.city').text(`${responseForecast.name} Weather details`);
+//   $('.humidity').text(`Humidity: ${responseForecast.main.humidity}%`);
+//   $('.wind').text(`Wind speed: ${responseForecast.wind.speed} MPH`);
+//   // Create CODE HERE to calculate the temperature (converted from Kelvin)
+//   const f2 = (responseForecast.main.temp - 273.15) * 1.8 + 32;
+//   // Hint: To convert from Kelvin to Fahrenheit: F = (K - 273.15) * 1.80 + 32
+// //   SK - use toFixed to make it go to 2 decimal places
+//   $('.temp').text(`Temperature: ${f2.toFixed(2)} F`);
+//   // Create CODE HERE to dump the temperature content into HTML
+// });
+
+}
 });
