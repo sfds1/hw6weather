453035f1e313cad55e093c531c8ec521aee8de26
SK <stephanie@sfdigitalstudio.com>
2020-02-15 22:24:35 -0800

    got 1 forecast value

--- assets/script.js
+++ assets/script.js
@@ -60,30 +60,45 @@ $("#submit").on("click", function(event){
 
 
 
-// // The URL to get the 5 day forecast weather
-// var queryURLForecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + inputCity + "," + inputState + ",US&appid=" + APIKey;
+// The URL to get the 5 day forecast weather
+var queryURLForecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + inputCity + "," + inputState + ",US&appid=" + APIKey;
+var index =0;
+// We then create an AJAX call
+$.ajax({
+  url: queryURLForecast,
+  method: "GET"
+}).then(function(responseForecast) {
+  // Create CODE HERE to Log the queryURL
+  console.log(queryURLForecast);
+  console.log(responseForecast);      
 
-// // We then created an AJAX call
-// $.ajax({
-//   url: queryURLForecast,
-//   method: "GET"
-// }).then(function(responseForecast) {
-//   // Create CODE HERE to Log the queryURL
-//   console.log(queryURLForecast);
-//   console.log(responseForecast);      
+var forecastDate = new Date(responseForecast.list[index].dt_txt).toDateString();
+
 //   // Create CODE HERE to log the resulting object
 //   // Create CODE HERE to transfer content to HTML
-//   $('.date').text(today);
+  $('.dateForecast').text(forecastDate);
 //   $('.city').text(`${responseForecast.name} Weather details`);
-//   $('.humidity').text(`Humidity: ${responseForecast.main.humidity}%`);
+  $('.humidityForecast').text(`Humidity: ${responseForecast.list[index].main.humidity}%`);
 //   $('.wind').text(`Wind speed: ${responseForecast.wind.speed} MPH`);
 //   // Create CODE HERE to calculate the temperature (converted from Kelvin)
-//   const f2 = (responseForecast.main.temp - 273.15) * 1.8 + 32;
+  const f2 = (responseForecast.list[index].main.temp - 273.15) * 1.8 + 32;
 //   // Hint: To convert from Kelvin to Fahrenheit: F = (K - 273.15) * 1.80 + 32
 // //   SK - use toFixed to make it go to 2 decimal places
-//   $('.temp').text(`Temperature: ${f2.toFixed(2)} F`);
+  $('.tempForecast').text(`Temperature: ${f2.toFixed(2)} F`);
 //   // Create CODE HERE to dump the temperature content into HTML
-// });
+
+
+});
 
 }
 });
+
+
+// Create input fields for city and State
+// create variables for the city and state input
+// Ajax call to openweather API to get the current weather information
+// Display basic weather information for current day
+// Need to pull longitude and latitude information from first Ajax call to put in to UV Index ajax call
+// Display UVIndex for current day
+// Run ajax call for 5 day forecast
+// store inputs in to local storage
